044ab9c4713292d467364dc2eb7a651b6bd82cf8
ValentinaHutter <valentina.hutter@eodc.eu>
2025-12-15 09:02:35 +0100

    update xr indices

--- indices_compute/load_bands/indices.py
+++ indices_compute/load_bands/indices.py
@@ -8,11 +8,12 @@ def evi(cube_10, cube_20=None):
     b08 = cube_10.nir.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return (2.5*(b08 - b04)/((b08 + 6*b04-7.5*b02) + 1)).where(NaN, np.nan).astype("float16")
+
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, (2.5*(b08 - b04)/((b08 + 6*b04-7.5*b02) + 1)), np.nan).astype("float16")
+        return np.where(NaN, (2.5*(b08 - b04)/((b08 + 6*b04-7.5*b02) + 1)), np.nan).astype("float16")
 
 
 def nbr(cube_10, cube_20):
@@ -21,11 +22,11 @@ def nbr(cube_10, cube_20):
     b12 = cube_20.swir22.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return ((b08 - b12)/(b08 + b12)).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, ((b08 - b12)/(b08 + b12)), np.nan).astype("float16")
+        return np.where(NaN, ((b08 - b12)/(b08 + b12)), np.nan).astype("float16")
 
 
 def ndmi(cube_10, cube_20):
@@ -34,11 +35,11 @@ def ndmi(cube_10, cube_20):
     b11 = cube_20.swir16.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return ((b08 - b11)/(b08 + b11)).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, ((b08 - b11)/(b08 + b11)), np.nan).astype("float16")
+        return np.where(NaN, ((b08 - b11)/(b08 + b11)), np.nan).astype("float16")
 
 
 def nmdi(cube_10, cube_20):
@@ -48,11 +49,11 @@ def nmdi(cube_10, cube_20):
     b12 = cube_20.swir22.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return ((b08 - (b11 - b12))/(b08 + (b11 - b12))).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, ((b08 - (b11 - b12))/(b08 + (b11 - b12))), np.nan).astype("float16")
+        return np.where(NaN, ((b08 - (b11 - b12))/(b08 + (b11 - b12))), np.nan).astype("float16")
 
 
 def ndwi(cube_10, cube_20=None):
@@ -61,11 +62,11 @@ def ndwi(cube_10, cube_20=None):
     b08 = cube_10.nir.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return ((b03 - b08)/(b08 + b03)).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, ((b03 - b08)/(b08 + b03)), np.nan).astype("float16")
+        return np.where(NaN, ((b03 - b08)/(b08 + b03)), np.nan).astype("float16")
 
 
 def ndii(cube_10, cube_20):
@@ -74,11 +75,11 @@ def ndii(cube_10, cube_20):
     b11 = cube_20.swir16.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return ((b08 - b11)/(b08 + b11)).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, ((b08 - b11)/(b08 + b11)), np.nan).astype("float16")
+        return np.where(NaN, ((b08 - b11)/(b08 + b11)), np.nan).astype("float16")
 
 
 def exg(cube_10, cube_20=None):
@@ -88,11 +89,11 @@ def exg(cube_10, cube_20=None):
     b04 = cube_10.red.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return ((2 * b03) - (b04 + b02)).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, ((2 * b03) - (b04 + b02)), np.nan).astype("float16")
+        return np.where(NaN, ((2 * b03) - (b04 + b02)), np.nan).astype("float16")
     
 
     
@@ -105,11 +106,11 @@ def tcari_osavi(cube_10, cube_20):
     b05 = cube_20.rededge1.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return (3*((b05-b04) - 0.2*(b05-b03)*(b05/4))/(1.16*b08 - (b04/b08) + b04 + 0.16)).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, (3*((b05-b04) - 0.2*(b05-b03)*(b05/4))/(1.16*b08 - (b04/b08) + b04 + 0.16)), np.nan).astype("float16")
+        return np.where(NaN, (3*((b05-b04) - 0.2*(b05-b03)*(b05/4))/(1.16*b08 - (b04/b08) + b04 + 0.16)), np.nan).astype("float16")
     
     
 def ndvi(cube_10, cube_20=None):
@@ -118,11 +119,11 @@ def ndvi(cube_10, cube_20=None):
     b08 = cube_10.nir.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return ((b08 - b04)/(b08 + b04)).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, ((b08 - b04)/(b08 + b04)), np.nan).astype("float16")
+        return np.where(NaN, ((b08 - b04)/(b08 + b04)), np.nan).astype("float16")
     
     
 def albedo(cube_10, cube_20):
@@ -138,11 +139,11 @@ def albedo(cube_10, cube_20):
     b12 = cube_20.swir22.astype(int)*0.0001-0.1
 
     if isinstance(cube_10.red, xr.DataArray):
-        NaN = xr.where((cube_10.red == 0), False, True)
+        NaN = (cube_10.red == 0).astype("bool")
+        return (b02*0.1836 + b03*0.1759 + b04*0.1456 + b05*0.1347 + b06*0.1233 + b07*0.1134 + b08*0.1001 + b11*0.0231 + b12*0.0003).where(NaN, np.nan).astype("float16")
     elif isinstance(cube_10.red, np.ndarray):
         NaN = np.where((cube_10.red == 0), False, True)
-
-    return np.where(NaN, (b02*0.1836 + b03*0.1759 + b04*0.1456 + b05*0.1347 + b06*0.1233 + b07*0.1134 + b08*0.1001 + b11*0.0231 + b12*0.0003), np.nan).astype("float16")
+        return np.where(NaN, (b02*0.1836 + b03*0.1759 + b04*0.1456 + b05*0.1347 + b06*0.1233 + b07*0.1134 + b08*0.1001 + b11*0.0231 + b12*0.0003), np.nan).astype("float16")
     
     
 def get_bands(cube_10, cube_20):
